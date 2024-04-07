218098cb99a068c1e7108cb98f5ed2ab8141747d
Riwan William Bououli <riwan.b@live.fr>
2024-04-07 21:16:27 +0200

    good bye authentification

--- middleware/isAuthenticated.js
+++ middleware/isAuthenticated.js
@@ -1,23 +1,23 @@
-const Users = require('../models/Users');
+// const Users = require('../models/Users');
 
-const isAuthenticated = async (req, res, next) => {
-   try {
-      if (!req.headers.authorization) {
-         return res.status(401).json({ message: error.message });
-      }
-      const token = req.headers.authorization.replace('Bearer ', '');
-      const user = await Users.findOne({ token: token }).select(
-         'email token _id'
-      );
-      if (!user) {
-         return res.status(401).json({ message: error.message });
-      }
-      req.user = user;
-      next();
-   } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: error.message });
-   }
-};
+// const isAuthenticated = async (req, res, next) => {
+//    try {
+//       if (!req.headers.authorization) {
+//          return res.status(401).json({ message: error.message });
+//       }
+//       const token = req.headers.authorization.replace('Bearer ', '');
+//       const user = await Users.findOne({ token: token }).select(
+//          'email token _id'
+//       );
+//       if (!user) {
+//          return res.status(401).json({ message: error.message });
+//       }
+//       req.user = user;
+//       next();
+//    } catch (error) {
+//       console.error(error);
+//       res.status(500).json({ message: error.message });
+//    }
+// };
 
-module.exports = isAuthenticated;
+// module.exports = isAuthenticated;
