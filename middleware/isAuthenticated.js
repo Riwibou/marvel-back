78a1b6991f2119e8fca47b6686094b0e1e584178
Riwan William Bououli <riwan.b@live.fr>
2024-04-07 11:32:50 +0200

    fixing isAuthenticated by adding a try catch

--- middleware/isAuthenticated.js
+++ middleware/isAuthenticated.js
@@ -1,17 +1,23 @@
 const Users = require('../models/Users');
 
 const isAuthenticated = async (req, res, next) => {
-   if (!req.headers.authorization) {
-      return res.status(401).json({ message: error.message });
+   try {
+      if (!req.headers.authorization) {
+         return res.status(401).json({ message: error.message });
+      }
+      const token = req.headers.authorization.replace('Bearer ', '');
+      const user = await Users.findOne({ token: token }).select(
+         'email token _id'
+      );
+      if (!user) {
+         return res.status(401).json({ message: error.message });
+      }
+      req.user = user;
+      next();
+   } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: error.message });
    }
-   const token = req.headers.authorization.replace('Bearer ', '');
-   const user = await Users.findOne({ token: token }).select(
-      'username token _id'
-   );
-   if (!user) {
-      return res.status(401).json({ message: error.message });
-   }
-   req.user = user;
-   next();
 };
+
 module.exports = isAuthenticated;
